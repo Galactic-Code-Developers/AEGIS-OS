2b98263eb66b6889ef7abfcc62db3378239d2897
Antonios Valamontes <8299759+geopayme@users.noreply.github.com>
2025-11-25 19:00:38 +0200

    Update realm.rs

--- kernel/src/realm.rs
+++ kernel/src/realm.rs
@@ -1,4 +1,15 @@
-pub fn test_spawn() {
+#[repr(C)]
+struct RealmDescriptor {
+    ipa: u64,
+    size: u64,
+    flags: u64,
+}
+
+pub fn spawn_compiler() {
     println!("[AEGIS] Spawning confidential Realm: compiler");
     println!("[AEGIS] Realm sealed – kernel cannot read its RAM");
+}
+
+pub fn spawn_shell() {
+    println!("[AEGIS] Spawning confidential Realm: shell (you are here)");
 }
